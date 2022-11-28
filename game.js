89e94fc24c8b7b6ab2d9b9f4c8c63ae228e09752
carolinekle <caroline.klewinowski@gmail.com>
2022-11-28 18:19:18 -0500

    mMessed about, unsuccessful

---
+++ game.js
@@ -0,0 +1,6 @@
+let choices = ["Rock" , "Paper" , "Scissors"];
+function getComputerChoice(choices){
+    var index = Math.floor(Math.random() * choices.length);
+}
+
+console.log(getComputerChoice);
